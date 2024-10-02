e0a46ad4224674ad16ce5e1823d0fb74baf91ea9
KmEnriquez <kentmathewenriquez@gmail.com>
2024-10-02 12:54:28 +0800

    update

--- js/slider.js
+++ js/slider.js
@@ -19,10 +19,3 @@ var slider = tns({
   }
 });
 
-const toggleMenu =document.querySelector(".header__toggleMenu");
-const headerNav = document.querySelector(".header__nav");
-
-toggleMenu.addEventListener("click", () =>{
-  toggleMenu.classList.toggle("open");
-  headerNav.classList.toggle("open");
-})
